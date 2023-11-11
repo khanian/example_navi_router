39f64c89c782ca4f42ce915e6b9e4e0e5ff07dfc
khanian <khanylee@gmail.com>
2023-11-11 11:13:00 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/example_navi_router/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.example_navi_router
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:example_navi_router/screen/new_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomeWidget(),
+  ));
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter에서 화면 이동하기'),
+      ),
+      body: Center(
+        child: TextButton(
+          child: Text('Go go Page'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const NewPage(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
